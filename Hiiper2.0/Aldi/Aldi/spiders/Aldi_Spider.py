90960076d2e78dfd36c2a17e68fd885673e41453
Bostan Dan George <74505830+Bosti2101@users.noreply.github.com>
2021-11-08 15:30:50 +0200

    Update Aldi_Spider.py

--- Hiiper2.0/Aldi/Aldi/spiders/Aldi_Spider.py
+++ Hiiper2.0/Aldi/Aldi/spiders/Aldi_Spider.py
@@ -4,25 +4,25 @@ import scrapy
 class AldiSpiderSpider(scrapy.Spider):
     name = 'Aldi_Spider'
     allowed_domains = ['aldi.nl/producten.html']
-    start_urls = ['http://aldi.nl/producten.html']
+    start_urls = ['https://www.aldi.nl/producten.html']
 
     def parse(self, response):
-        for link in response.css('div.tiles-grid a::attr(href)'):
-            yield response.follow('aldi.nl' + link.get(), callback=self.parse_categories)
+        for link in response.css('.mod-content-tile__action::attr(href)'):
+            yield response.follow(link.get(), callback=self.parse_subcategories)
 
     def parse_subcategories(self, response):
-        for link in response.css('div.tiles-grid a::attr(href)'):
-            yield response.follow('aldi.nl' + link.get(), callback=self.parse_products)
+        for link in response.css('.mod-content-tile__action::attr(href)'):
+            yield response.follow(link.get(), callback=self.parse_products)
 
     def parse_products(self, response):
-        for link in response.css('div.tiles-grid a::attr(href)'):
-            yield response.follow('aldi.nl' + link.get(), callback=self.parse_data)
+        for link in response.css('.mod-article-tile__action::attr(href)'):
+            yield response.follow(link.get(), callback=self.parse_data)
 
     def parse_data(self, response):
         datas = response.css('div.container')
         for data in datas:
             yield {
-                'product_name': data.css('span.mod-article-intro__header-headline-small::text').get().strip(),
+                'product_name': '',
                 'price': data.css('span.price__wrapper::text').get().strip(),
-                'description': data.css('div.mod-article-intro__info::text').get().strip(),
+                'description': '',
             }
