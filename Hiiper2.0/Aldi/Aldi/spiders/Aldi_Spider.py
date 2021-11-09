b23894a949a0d8131d4979eeac95444e652cd735
Bostan Dan George <74505830+Bosti2101@users.noreply.github.com>
2021-11-09 17:20:45 +0200

    Update Aldi_Spider.py

--- Hiiper2.0/Aldi/Aldi/spiders/Aldi_Spider.py
+++ Hiiper2.0/Aldi/Aldi/spiders/Aldi_Spider.py
@@ -21,23 +21,43 @@ class AldiSpiderSpider(scrapy.Spider):
             yield response.follow(link.get(), callback=self.parse_data)
 
     def parse_data(self, response):
+
         info1 = response.css('.mod-article-intro::attr(data-article)').get()
         info2 = json.loads(info1)
+
         today1 = datetime.datetime.now().strftime("%x")
         time1 = datetime.datetime.now().strftime("%X")
+
+        data = response.css('span.price__unit::text').get().strip()
+
+        new_data = ''.join((ch if ch in 'abcdefghijklmnoupPqrstvxyz' else ' ')
+                           for ch in data)
+        amount1 = [i for i in new_data.split()]
+
+        new_data2 = ''.join((ch if ch in '1234567890x/' else ' ')
+                            for ch in data)
+        quantity1 = [i for i in new_data2.split()]
+
+        price1 = info2['productInfo']['priceWithTax']
+        if(price1 == -1.0):
+            price1 = "Out of Stock"
+
+        description1 = response.css('.rte > p::text').get()
+        if(description1 == "Ondanks zorgvuldige planning kan het voorkomen dat actie-artikelen door de grote vraag snel zijn uitverkocht. Mochten de actie-artikelen sneller uitverkocht zijn dan voorzien, dan vragen wij hiervoor begrip. Er geldt maximaal 4 dezelfde (non-food) aanbiedingen per klant per bezoek. De afgebeelde artikelen kunnen afwijken van de werkelijkheid." or description1 == "U ontvangt van ons een e-mail om uw e-mailadres te bevestigen. Klik op de link in de e-mail."):
+            description1 = "No Description"
 
         yield {
             'shop': 'Aldi',
             'product_id': info2['productInfo']['productID'],
             'product_name': info2['productInfo']['productName'],
-            'price': info2['productInfo']['priceWithTax'],
-            'description': '',
-            'images': '',
+            'price': price1,
+            'description': description1,
+            'images': response.css('a.mod-gallery-article__media::attr(href)').get(),
             'category_level_1': info2['productCategory']['primaryCategory'],
             'category_level_2': info2['productCategory']['subCategory1'],
-            'quantity': response.css('span.price__unit::text').get().strip(),
-            'amount': '',
-            'url': 'aldi.nl' + info2['id'],
+            'quantity': quantity1[0],
+            'amount': amount1[0],
+            'url': response.url,
             'date': today1,
             'time': time1,
         }
